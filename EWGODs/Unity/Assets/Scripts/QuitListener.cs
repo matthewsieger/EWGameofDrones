f27a3e5e1ff3d5173f3c51bb173b5cee9b75f751
WilliamBlack99 <wblack10055@gmail.com>
2023-07-20 10:40:33 -0700

    fixed quitting again
    in order to test I have to upload to remote to get to testing computer unfortunately

--- EWGODs/Unity/Assets/Scripts/QuitListener.cs
+++ EWGODs/Unity/Assets/Scripts/QuitListener.cs
@@ -7,14 +7,17 @@ public class QuitListener : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+		// listen to quit on all scenes
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+		// quit if ESC pressed
         if (Input.GetKeyUp(KeyCode.Escape))
 		{
+			// quit
 			Application.Quit();
 		}
     }
